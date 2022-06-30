f237a0a21d0dfcde8e1ec7b28a4a34a0ccee12d3
MosNes <7hekarl@gmail.com>
2022-06-30 10:41:19 -0400

    tested and added .getStats and .getInventory methods to the Player constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -6,10 +6,35 @@ jest.mock('../lib/Potion');
 test('creates a player object', () => {
     const player = new Player('Karlos');
 
+    //Note: best practice is to use .toEqual for everything instead of .toBe
     expect(player.name).toBe('Karlos');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
 
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+test("gets player's stats as an object", () => {
+    const player = new Player('MosNes');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+    
+    console.log("Player Stats: ", player.getStats());
+});
+
+test("gets inventory from player or returns false", () => {
+    const player = new Player('MosNes');
+
+    console.log("full inv: ", player.getInventory());
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    //simulates empty inventory
+    player.inventory = [];
+    
+    console.log("empty inv: ", player.getInventory());
+    expect(player.getInventory()).toEqual(false);
 });
